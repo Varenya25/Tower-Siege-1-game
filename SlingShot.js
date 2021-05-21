ddf06c8393eb652051d8d8094a93932dbb39385d
Varenya25 <76617004+Varenya25@users.noreply.github.com>
2021-05-21 18:35:05 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,37 @@
+class SlingShot {
+    constructor (bodyX,pointY) {
+         var options = {
+            bodyA : bodyX,
+            pointB : pointY,
+            stiffness : 0.04,
+            length : 20
+        }
+        
+        this.slingShot = new Constraint.create (options);
+        World.add(world,this.slingShot);
+    }
+
+    display () {
+
+        if(this.slingShot.bodyA != null){
+            var point1 = this.slingShot.bodyA.position;
+            var point2 =  this.slingShot.pointB;
+
+            push();
+            strokeWeight(7);
+            stroke(42,28,8);
+            line(point1.x,point1.y,point2.x,point2.y);
+            pop();
+
+            
+          }
+        }
+
+    fly()  {
+
+     this.slingShot.bodyA = null;
+
+    }
+
+
+}
